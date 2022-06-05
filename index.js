27c663f3614a00b6abb73845a5ec55c1a7c3ef2b
derrique <otieno.derrick@student.moringaschool.com>
2022-06-05 21:33:49 +0300

    complted assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+function introduction(name) {
+return name = `Hi, my name is ${name}.`
+}
+
+function introductionWithLanguageOptional(name, language) {
+  return name, language = `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+
+function introductionWithLanguage(name, language='JavaScript') {
+  return name, language = `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+
+function introductionWithLanguageOptional(name, language='JavaScript') {
+  return name, language = `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
